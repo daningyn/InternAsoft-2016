610b4501a5fdd631d5d0da3c6c6a7603ed76548b
Danh Nguyen <danh.nghuu@gmail.com>
2016-12-14 10:05:16 +0700

    - Clear NSCountedSet

--- Data Structures And Algorithm/Intern-Asoft-2016-NSCountedSet.playground/Contents.swift
+++ Data Structures And Algorithm/Intern-Asoft-2016-NSCountedSet.playground/Contents.swift
@@ -3,3 +3,27 @@
 import UIKit
 
 var str = "Hello, NSCountedSet"
+
+// Initialization
+
+let array = ["Danh", "Toan", "Minh"]
+let arraySet: Set<String> = ["Danh", "Toan", "Minh"]
+let countedSet = NSCountedSet(array: array)
+let countedSet1 = NSCountedSet(set: arraySet)
+var countedSet2 = NSCountedSet(capacity: 2)
+
+// Methods
+// Add object
+countedSet.add("Hau")
+countedSet.add("Hau")
+
+// Remove object
+countedSet.remove("Danh")
+
+// Count object
+countedSet.count(for: "Hau") // 2
+
+// Using method objectEnumerator to access each object
+for i in countedSet.objectEnumerator() {
+    print("object: \(i)")
+}
