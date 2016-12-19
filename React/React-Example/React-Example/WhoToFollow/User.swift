9ff9b892dc3fa4b591661a13b2b99d8810eaaeff
Danh Nguyen <danh.nghuu@gmail.com>
2016-12-19 17:14:21 +0700

    + Create List Example + Create WhoToFollow example

---
+++ React/React-Example/React-Example/WhoToFollow/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  React-Example
+//
+//  Created by Danh Nguyen on 12/19/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import Foundation
---
+++ React/React-Example/React-Example/WhoToFollow/WhoToFollowTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WhoToFollowTableViewCell.swift
+//  React-Example
+//
+//  Created by Danh Nguyen on 12/19/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class WhoToFollowTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
