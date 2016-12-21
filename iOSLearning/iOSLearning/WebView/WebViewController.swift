91a9e9c06297319e4e94f3c5aa09267457b388a8
Danh Nguyen <danh.nghuu@gmail.com>
2016-12-21 15:23:08 +0700

    + Create Web View

---
+++ iOSLearning/iOSLearning/WebView/WebViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WebViewController.swift
+//  iOSLearning
+//
+//  Created by Danh Nguyen on 12/21/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
