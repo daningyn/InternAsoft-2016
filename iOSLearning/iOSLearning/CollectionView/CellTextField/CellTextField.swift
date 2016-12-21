e1e761724909633bd21488527bb77d2548ff35db
Danh Nguyen <danh.nghuu@gmail.com>
2016-12-21 14:39:41 +0700

    + Create Cell for CollectionView

---
+++ iOSLearning/iOSLearning/CollectionView/CellTextField/CellTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CellTextField.swift
+//  iOSLearning
+//
+//  Created by Danh Nguyen on 12/21/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class CellTextField: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
